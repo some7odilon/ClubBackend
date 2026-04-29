041cc8621d69b9fe8a7eff5b79a98a2f46a7d9b1
some7odilon <somebodilon@gmail.com>
2026-04-29 12:10:13 +0000

    Fix CORS: ensure CorsMiddleware is first and origins are correct

--- Backend/settings.py
+++ Backend/settings.py
@@ -70,6 +70,29 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_CREDENTIALS = True
 
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
+
 ROOT_URLCONF = 'Backend.urls'
 
 
